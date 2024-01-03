9c8d0f643bedd1d41ff1301e4f30a2cd0143baff
ibraevadilet <ibraevadilet3@gmail.com>
2024-01-03 22:32:22 +0600

    add

--- lib/features/settings/settings_page.dart
+++ lib/features/settings/settings_page.dart
@@ -2,7 +2,7 @@ import 'package:bet_calculator/core/image/app_images.dart';
 import 'package:bet_calculator/core/premium/premium.dart';
 import 'package:bet_calculator/core/urls.dart';
 import 'package:bet_calculator/features/settings/widget/settings_container_widget.dart';
-import 'package:bet_calculator/theme/app_text_styles.dart';
+import 'package:bet_calculator/widgets/custom_app_bar.dart';
 import 'package:bet_calculator/widgets/web_view_bet_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
@@ -30,12 +30,8 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Settings',
-          style: AppTextStylesBetCalculator.s24W600(),
-        ),
-      ),
+      backgroundColor: Colors.white,
+      appBar: const CustomAppBarBetCalculator(title: 'Settings'),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
--- lib/features/trhee_page/trhee_page.dart
+++ lib/features/trhee_page/trhee_page.dart
@@ -2,8 +2,10 @@ import 'package:bet_calculator/features/trhee_page/cubit/referees_cubit.dart';
 import 'package:bet_calculator/features/trhee_page/refery_detail_page.dart';
 import 'package:bet_calculator/theme/app_text_styles.dart';
 import 'package:bet_calculator/widgets/custom_app_bar.dart';
+import 'package:bet_calculator/widgets/spaces.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class TrheePage extends StatelessWidget {
   const TrheePage({super.key});
@@ -18,51 +20,47 @@ class TrheePage extends StatelessWidget {
           bloc: RefereesCubit()..getData(),
           builder: (context, state) {
             if (state is RefereesLoaded) {
-              return Padding(
+              return MasonryGridView.count(
                 padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: GridView.builder(
-                  itemCount: state.data.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 0.73,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20),
-                  itemBuilder: (context, index) => InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  ReferyDetailPage(data: state.data[index])));
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          clipBehavior: Clip.antiAlias,
-                          height:
-                              (MediaQuery.of(context).size.width - 68) / 2 + 20,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.grey),
-                          child: Image.network(
-                            state.data[index].image!,
-                            fit: BoxFit.fitWidth,
-                            width: (MediaQuery.of(context).size.width - 68) / 2,
-                          ),
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                shrinkWrap: true,
+                itemCount: state.data.length,
+                crossAxisCount: 2,
+                itemBuilder: (context, index) => InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                ReferyDetailPage(data: state.data[index])));
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        clipBehavior: Clip.antiAlias,
+                        height: (context.width - 68) / 2 + 20,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey),
+                        child: Image.network(
+                          state.data[index].image!,
+                          fit: BoxFit.fitWidth,
+                          width: (context.width - 68) / 2,
                         ),
-                        const SizedBox(height: 5),
-                        Text(
-                          state.data[index].name!,
-                          style: AppTextStylesBetCalculator.s14W600(),
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          state.data[index].type!,
-                          style: AppTextStylesBetCalculator.s12W400(),
-                        ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        state.data[index].name!,
+                        style: AppTextStylesBetCalculator.s14W600(),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        state.data[index].type!,
+                        style: AppTextStylesBetCalculator.s12W400(),
+                      ),
+                    ],
                   ),
                 ),
               );
